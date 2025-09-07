d8946d8b46d7be2eb2549db6eb0abe4fdc9aca09
Temesgen <temesgenabdissa2@gmail.coom>
2025-09-07 09:49:31 +0300

    Header and Footer of the Projects

--- Netflix_Clone/src/App.jsx
+++ Netflix_Clone/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Netflix Clone</h1>
+      <div className='App'>
+       <Home/>
       </div>
       
     </>
