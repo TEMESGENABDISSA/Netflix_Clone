9b7b0a8c9e55ebc027c708f4ae7f7bb5c0d99451
Temesgen <temesgenabdissa2@gmail.coom>
2025-09-08 00:58:42 +0300

    Prepare for gh-pages deployment

--- Netflix_Clone/vite.config.js
+++ Netflix_Clone/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  base: "/Netflix_Clone/", // <-- change this to "/your-repo-name/" if different
   plugins: [react()],
-})
+});
