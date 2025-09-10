a843301008f86e243f5f2bc6b82bb2dc7617ca46
Temesgen <temesgenabdissa2@gmail.coom>
2025-09-10 21:55:45 +0300

    fix the header codes

--- Netflix_Clone/src/Components/Header/Header.jsx
+++ Netflix_Clone/src/Components/Header/Header.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import './Header.css'
+import React, { useEffect, useState } from "react";
+import "./Header.css";
 import NetflixLogo from "../../assets/images/NetflixLogo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import PersonIcon from "@mui/icons-material/Person";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
 const Header = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // cleanup
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="header_Outer_Container">
+    <div className={`header_Outer_Container ${show ? "nav__black" : ""}`}>
       <div className="header_container">
         <div className="header_left">
           <ul>
             <li>
-              {" "}
-              <img src={NetflixLogo} alt="Netfilx Logo" width="100" />{" "}
+              <img src={NetflixLogo} alt="Netflix Logo" width="100" />
             </li>
             <li>Netflix</li>
             <li>Home</li>
@@ -43,6 +60,6 @@ const Header = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
